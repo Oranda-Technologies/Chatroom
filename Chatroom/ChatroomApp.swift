64ce74222596915ea3b560ec1ffacf86d832de2b
Chimwemwe Phikiso <97michelangelo@gmail.com>
2022-05-07 19:31:50 +0100

    Initial Commit

---
+++ Chatroom/ChatroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatroomApp.swift
+//  Chatroom
+//
+//  Created by Chimwemwe Phikiso on 07/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
